a61c2cfc94b092aebda39b40f5cfbc420380517a
uo269787 <60620685+uo269787@users.noreply.github.com>
2021-03-07 02:08:56 +0100

    Change webapp for react-native expo initial project
    Docker image adapted to the new build method with expo.
    
    Load testing example with gatling was removed.
    
    Removed now unused files from src/

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,39 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
-import Welcome from './components/Welcome';
-import EmailForm from "./components/EmailForm";
-import UserList from "./components/UserList";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {users:[]}
-  }
 
-  refreshUsers(users){
-    this.setState({users:users})
-  }
-
-  render(){
-    return(
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <Welcome name="ASW students"/>
-        </header>
-        <div className="App-content">
-          <EmailForm refreshUsers={this.refreshUsers.bind(this)}/>
-          <UserList users={this.state.users}/>
-          <a className="App-link"
-            href="https://github.com/pglez82/radarin_0"
-            target="_blank"
-            rel="noopener noreferrer">Source code</a>
-        </div>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+registerRootComponent(App);
