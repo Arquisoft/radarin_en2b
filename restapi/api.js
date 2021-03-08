e171f51b442134cca04ab14e6d37efaafabd3e2e
uo270115 <uo270115@uniovi.es>
2021-03-09 00:24:00 +0100

    Working on RestAPI and MongoDB

--- restapi/api.js
+++ restapi/api.js
@@ -3,27 +3,60 @@ const User = require("./models/users")
 const router = express.Router()
 
 // Get all users
-router.get("/users/list", async (req, res) => {
-    const users = await User.find({}).sort('-_id') //Inverse order
+router.get("user/list", async (req, res) => {
+    const users = await User.find({}).sort('webId')
+    //console.log(users)
 	res.send(users)
 })
 
-//register a new user
-router.post("/users/add", async (req, res) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    //Check if the device is already in the db
-    let user = await User.findOne({ email: email })
-    if (user)
+// Register a new user
+router.post("user/add", async (req, res) => {
+    let webId = req.body.webId;
+    let location = req.body.location;
+    let authKey = req.body.authKey;
+    //Check if the user is already in the db
+    let user = await User.findOne({ webId: webId })
+    if (user){
         res.send({error:"Error: This user is already registered"})
-    else{
+    }else{
         user = new User({
-            name: name,
-            email: email,
+            webId: webId,
+            location: location,
+            authKey: authKey
         })
         await user.save()
         res.send(user)
     }
+})
+
+// Submit user's location
+router.post("user/location/submit", async (req, res) => {
+    let webId = req.body.webId;
+    let location = req.body.location;
+    let authKey = req.body.authKey;
+    //Check if the device is already in the db
+    let user = await User.findOne({ webId: webId })
+    if (user){
+        user.location = location
+        console.log("User's location updated")     
+    }
+    // an else is not needed because when a user signs up (/user/add)
+    // the user is created and the location is saved
+    await user.save()
+    res.send(user)
+})
+
+// Find the user's friends that are near
+router.post("user/location/near", async (req, res) => {
+    let userLocation = req.body.userLocation;
+    let userFriends = req.body.friends; // subcollection of User
+    let userNearByFriends = await userFriends.find({ location: {
+                                                    $near: userLocation,
+                                                    $minDistance: 10, // meters
+                                                    $maxDistance: 100
+                                                } 
+                                            })
+    res.send(userNearByFriends)
 })
 
 module.exports = router
--- restapi/models/users.js
+++ restapi/models/users.js
@@ -1,9 +1,53 @@
+// import the mongoose moduke
 const mongoose = require("mongoose")
 
+// import the specific GeoJson
+require('mongoose-geojson-schema')
 
-const schema = mongoose.Schema({
-    name: String,
-    email: String
-})
+/*
+// My mongoose local database 
+// to handle initial connection errors
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useUnifiedTopology: true}).catch(error => handleError(error));
 
-module.exports = mongoose.model("User", schema)
+// to handle errors after initial connection was established, we should listen for error events on the connection.
+mongoose.connection.on('error', err => {
+    logError(err);
+});*/
+
+const schema = mongoose.Schema(
+    {
+        //_id: mongoose.Schema.Types.ObjectId,
+        webId: String,
+        location: mongoose.Schema.Types.Point,
+        authKey: String
+    },
+    { timestamps: true } // one hour behind
+);
+
+module.exports = mongoose.model("User", schema) // User is a collection
+/*
+const User = mongoose.model("User", schema);
+
+const doc = new User({ 
+    //_id: new mongoose.Types.ObjectId(),
+    webId: "asdfghjklñ",
+    location: {
+        type: "Point",
+        coordinates: [43.53573, -5.66152 ]
+    }, 
+    authKey: "auth123456789"
+});
+
+doc.save(function(err) {
+    if (err) throw err;
+     
+    console.log('User succesfully saved.');
+
+    User.find({
+    }).sort('webId')
+    .exec(function (err, users){
+        if (err) throw err;
+         
+        console.log(users);
+    });
+});*/
