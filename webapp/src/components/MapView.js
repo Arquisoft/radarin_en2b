7da3ebede092dc52959f70ee72ab2de01571dc20
UO270185 <UO270185@uniovi.es>
2021-03-21 23:08:25 +0100

    Markers from a json file are shown in the map

--- webapp/src/components/MapView.js
+++ webapp/src/components/MapView.js
@@ -1,45 +1,30 @@
-import {React, Component} from 'react';
-import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
-
-class MapView extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      nearby: []
-    }
-  }
-
-  render() {
-    return (
-      <div style={{width: '100vw', height: '90vh', padding: '20px'}}>
-        <WrappedMap 
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
-            libraries=geometry,drawing,places&key=AIzaSyClIZED8kODn9vaGf-_ke73ETRNbFC9IhY`}
-          loadingElement={<div style={{height: '100%'}} /> } 
-          containerElement={<div style={{height: '100%'}} /> }
-          mapElement={<div style={{height: '100%'}} /> }
-        />
-      </div>
-    );
-  }
-
-  //Calling the RestAPI for the nearby friends
-  componentDidMount() {
-    fetch('/users/location/near')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ nearby: data })
-    })
-    .catch(console.log)
-  }
-}
+import React, {useState} from 'react';
+import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from 'react-google-maps';
+import FriendData from '../jsons/nearbyFriends.json';
 
 function Map() {
+  const [selectedFriend, setSelectedFriend] = useState(null);
   return (
     <GoogleMap defaultZoom={15} //Starting zoom and position
       defaultCenter={{lat: 1, lng: 1}}> 
-      
+      {FriendData.map((friend) => (
+        <Marker key={friend._id} position={{lat: friend.location.coordinates[0], 
+          lng: friend.location.coordinates[1]}}
+        onClick={() => {
+          setSelectedFriend(friend);
+        }}
+          />
+      ))}
+      {selectedFriend && (
+        <InfoWindow
+          position= {{lat: selectedFriend.location.coordinates[0],
+            lng: selectedFriend.location.coordinates[1]}} 
+          onCloseClick={() => {
+            setSelectedFriend(null);
+          }}>
+
+        </InfoWindow>
+      )}
     </GoogleMap>
   );
 }
@@ -47,4 +32,16 @@ function Map() {
 //Wrap the map so that react can handle it
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-export default MapView;
+export default function MapView() {
+  return (
+    <div style={{width: '100vw', height: '90vh', padding: '20px'}}>
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
+          libraries=geometry,drawing,places&key=AIzaSyClIZED8kODn9vaGf-_ke73ETRNbFC9IhY`}
+        loadingElement={<div style={{height: '100%'}} /> } 
+        containerElement={<div style={{height: '100%'}} /> }
+        mapElement={<div style={{height: '100%'}} /> }
+      />
+    </div>
+  );
+}
