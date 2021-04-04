4e3baceefe049828f7de2614232a149b7d11d063
UO270185 <UO270185@uniovi.es>
2021-04-04 13:01:48 +0200

    Changed the icon of the markers

--- webapp/src/components/MapView.js
+++ webapp/src/components/MapView.js
@@ -2,18 +2,21 @@ import React, {useState} from 'react';
 import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from 'react-google-maps';
 import FriendData from '../jsons/nearbyFriends.json';
 
+var markerImage = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/library_maps.png';
+
 function Map() {
   const [selectedFriend, setSelectedFriend] = useState(null);
   return (
     <GoogleMap defaultZoom={15} //Starting zoom and position
-      defaultCenter={{lat: 1, lng: 1}}> 
+      defaultCenter={{lat: 43.36029, lng: -5.84476}}> 
       {FriendData.map((friend) => (
-        <Marker key={friend._id} position={{lat: friend.location.coordinates[0], 
+        <Marker icon={markerImage} key={friend._id} position={{lat: friend.location.coordinates[0], 
           lng: friend.location.coordinates[1]}}
         onClick={() => {
           setSelectedFriend(friend);
         }}
-          />
+        //icon={{url: markerImage, scaledSize: new window.google.maps.Size(1000, 1000)}}
+        />
       ))}
       {selectedFriend && (
         <InfoWindow
@@ -21,8 +24,11 @@ function Map() {
             lng: selectedFriend.location.coordinates[1]}} 
           onCloseClick={() => {
             setSelectedFriend(null);
-          }}>
-
+          }}
+          >
+          <div>
+            <h2>{selectedFriend.webId}</h2>
+          </div>
         </InfoWindow>
       )}
     </GoogleMap>
