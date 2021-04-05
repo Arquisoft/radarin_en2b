f692e39e47af4adad055897ff5bf223da90f38ec
UO270185 <UO270185@uniovi.es>
2021-04-05 22:04:44 +0200

    Added api

---
+++ webapp/src/api/api.js
@@ -0,0 +1,64 @@
+//REACT_APP_API_URI is an enviroment variable defined in the file .env.development or .env.production
+async function getUserById(webId){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/getById", {
+        method: "POST",
+        headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "http://localhost:5000/api/users/getById", "Control-Allow-Methods": "POST"},
+        body: JSON.stringify({"webId": webId})
+    });
+    return await response.json();
+};
+
+async function removeUserById(webId){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/removeById", {
+        method: "POST",
+        headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "http://localhost:5000/api/users/getById", "Control-Allow-Methods": "POST"},
+        body: JSON.stringify({"webId": webId})
+    });
+    return await response.json();
+}
+
+async function getUsers(){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/list");
+    return await response.json();
+};
+
+async function getNormalUsers(){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/normal/list");
+    return await response.json();
+};
+
+async function addUser(webId, location, authKey){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/add", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"webId": webId, "location": location, "authKey": authKey})
+    });
+    return await response.json();
+};
+
+async function updateUserLocation(webId, location){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/location/submit", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"webId": webId, "location": location})
+    });
+    return await response.json();
+};
+
+async function getNearbyFriends(userLocation, friends){
+    const apiEndPoint= process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+    let response = await fetch(apiEndPoint + "/users/location/near", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"userLocation": userLocation, "friends": friends})
+    });
+    return await response.json();
+};
+
+export { getUserById, removeUserById, getUsers, getNormalUsers, addUser, updateUserLocation, getNearbyFriends }
