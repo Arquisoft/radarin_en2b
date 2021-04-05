8ca5ff0a9c409c115a59200175f9234800a06f15
uo269787 <60620685+uo269787@users.noreply.github.com>
2021-04-05 19:33:26 +0200

    Update mongo_db_uri to match docker default
    Add simple script to add an admin account to webapp
    
    Co-authored-by: UO270115 <UO270115@uniovi.es>

--- restapi/server.js
+++ restapi/server.js
@@ -8,7 +8,7 @@ const api = require("./api");
 
 function connect(){
     //The MONGO_URI variable is the connection string to MongoDB Atlas (for production). This env variable is created in heroku.
-    var mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017/myapp";
+    var mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017/api";
     mongoose.connect(mongo_uri, { useNewUrlParser: true,useUnifiedTopology: true }).then(() => {
         const app = express();
 
---
+++ restapi/setup-admin.js
@@ -0,0 +1,21 @@
+const User = require("./models/users");
+const mongoose = require("mongoose");
+
+var mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017/api";
+
+mongoose.connect(mongo_uri, { useNewUrlParser: true,useUnifiedTopology: true }).then(async () => {
+    console.log("Connected");
+    user = new User({
+        webId: process.env.ADMIN_WEBID,
+        location: {
+            "type": "Point",
+            "coordinates": [0.0, 0.0]
+        },
+        authKey: "unused",
+        role: "Admin"
+    });
+    console.log("User created");
+    await user.save();
+    console.log("Admin user saved");
+    process.exit();
+});
