cb4df6ef718a92a2200690d9e9e5bc56d7173d7b
Luis <UO271405@uniovi.es>
2021-04-07 20:17:26 +0200

    Style added to friend list view

--- webapp/src/components/FriendList.js
+++ webapp/src/components/FriendList.js
@@ -20,10 +20,10 @@ const Friends = () => {
 
   return(
     <div className='ml-3'>
-      <h2 className='mb-3'>Nearby friends</h2>
+      <h2 style={{marginTop: '10px', marginLeft: '40px'}}>Nearby friends</h2>
       {FriendData.map((friendDetail, index) => {
         return <div>
-          <ListGroup horizontal style={{marginTop: `20px`}}>
+          <ListGroup horizontal style={{marginTop: '20px', marginLeft: '40px'}}>
             <ListGroup.Item>
               <img src={saw} alt="saw"
                 width="80"
@@ -31,22 +31,27 @@ const Friends = () => {
                 className="d-inline-block align-top"
               />
             </ListGroup.Item>
-            <ListGroup.Item> <p align="center">{friendDetail.webId}</p><p align="center">
+            <ListGroup.Item style={{minWidth: '300px', minHeight: '100px'}}> 
+              <p align="center">
+                {friendDetail.webId}
+              </p>
+              <p align="center">
                 {geolib.getDistance({ latitude: 43.616541, longitude: -5.793476 }, { latitude: friendDetail.location.coordinates[0], longitude: friendDetail.location.coordinates[1] })}m away
-            </p> </ListGroup.Item>
-
-            <ListGroup.Item>
-              <Link to="/map">
-                <img src={lupa} alt="lupa"
-                  width="50"
-                  height="50"
-                  className="m-2"
-                />
-              </Link>
+              </p> 
             </ListGroup.Item>
 
+            <ListGroup.Item>
+                <Link to="/map">
+                  <img src={lupa} alt="lupa"
+                    width="50"
+                    height="50"
+                    className="m-3"
+                  />
+                </Link>
+              </ListGroup.Item>
+
             <ListGroup.Item >
-              <Dropdown className='m-3'>
+              <Dropdown className='m-4' >
                 <Dropdown.Toggle id="dropdown-basic">
 
                 </Dropdown.Toggle>
@@ -59,53 +64,55 @@ const Friends = () => {
           </ListGroup>
 
         </div>
-      })}
-      <h2 className='mb-3'>All friends</h2>
+        })
+      }
+      <h2 style={{marginTop: '10px', marginLeft: '40px'}}>All friends</h2>
       {activeProfile &&
-          <div>
-            <List src={`[${activeProfile}].friends`}>{friend =>
-                <ListGroup horizontal key={friend} style={{marginTop: `20px`}}>
-                  <ListGroup.Item horizontal>
-                    <img src={saw} alt="saw"
-                      width="80"
-                      height="80"
-                      className="d-inline-block align-top"
-                    />
-                  </ListGroup.Item>
-
-                  <ListGroup.Item> 
-                    <p align="center">
-                      <Value src={`[${friend}].name`}>{`${friend}`}</Value>
-                    </p>
-                  </ListGroup.Item>
-
-                  <ListGroup.Item>
-              <Link to="/map">
-                <img src={lupa} alt="lupa"
-                  width="50"
-                  height="50"
-                  className="m-2"
+        <div>
+          <List src={`[${activeProfile}].friends`}>{friend =>
+            <ListGroup horizontal key={friend} style={{marginTop: `20px`}}>
+              <ListGroup.Item horizontal style={{minWidth: '100px', minHeight: '100px'}}>
+                <img src={saw} alt="saw"
+                  width="80"
+                  height="80"
+                  className="d-inline-block align-top"
                 />
-              </Link>
-            </ListGroup.Item>
+              </ListGroup.Item>
 
-            <ListGroup.Item >
-              <Dropdown className='m-3'>
-                <Dropdown.Toggle id="dropdown-basic">
+              <ListGroup.Item style={{minWidth: '300px', minHeight: '100px'}}> 
+                <p align="center"><br></br>
+                  <Value src={`[${friend}].name`}>{`${friend}`}</Value>
+                </p>
+              </ListGroup.Item>
 
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item>Delete friend</Dropdown.Item>
-                  <Dropdown.Item>Info</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </ListGroup.Item>
+              <ListGroup.Item>
+                <Link to="/map">
+                  <img src={lupa} alt="lupa"
+                    width="50"
+                    height="50"
+                    className="m-3"
+                  />
+                </Link>
+              </ListGroup.Item>
 
-                  
-                </ListGroup>
-            }
-            </List>
-          </div>}
+              <ListGroup.Item >
+                <Dropdown className='m-4' >
+                  <Dropdown.Toggle id="dropdown-basic">
+
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Delete friend</Dropdown.Item>
+                    <Dropdown.Item>Info</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </ListGroup.Item>
+
+                
+            </ListGroup>
+          }
+          </List>
+        </div>
+      }
     </div>
   );
 }
