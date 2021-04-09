9020daf9a575d664be9768ed6f55debbfbc5a893
Luis <UO271405@uniovi.es>
2021-04-09 23:05:30 +0200

    Friends of current user almost added to array

--- webapp/src/components/FriendList.js
+++ webapp/src/components/FriendList.js
@@ -12,14 +12,44 @@ import FriendData from '../jsons/friendList.json'
 import { LoggedIn, LoggedOut, AuthButton, Value, List, withWebId } from '@solid/react';
 import { useSession } from '@inrupt/solid-ui-react';
 
+import { updateUserLocation, addUser, getUserById, getNearbyFriends } from '../api/api';
+
 const geolib = require('geolib');
 
 const Friends = () => {
   const{ session } = useSession();
   const [activeProfile] = useState(session.info.webId);
 
+  var friendWebId;
+  var friendsOfUser = new Array();
+  var counter = 0;
+
+  console.log('Hola');
+  
+  /*navigator.geolocation.getCurrentPosition(async function (position) {
+    await getNearbyFriends(position, friendsOfUser);
+  });*/
+
+  function hello(){
+    return (
+      <div>
+      {activeProfile &&
+      <dl>
+        <List src={`[${activeProfile}].friends`}>{friend =>
+          <li key={friend}>
+            {friendWebId = `${friend}`}
+            {friendsOfUser[counter] = friendWebId}
+            {counter++}
+          </li>}
+        </List>
+        {counter = 0}
+      </dl>}</div>);
+  }
+  
+
   return(
     <div className='ml-3'>
+      {hello()}
       <h2 style={{marginTop: '10px', marginLeft: '40px'}}>Nearby friends</h2>
       {FriendData.map((friendDetail, index) => {
         return <div>
@@ -37,7 +67,7 @@ const Friends = () => {
               </p>
               <p align="center">
                 {geolib.getDistance({ latitude: 43.616541, longitude: -5.793476 }, { latitude: friendDetail.location.coordinates[0], longitude: friendDetail.location.coordinates[1] })}m away
-              </p> 
+              </p>
             </ListGroup.Item>
 
             <ListGroup.Item>
@@ -72,19 +102,18 @@ const Friends = () => {
           <List src={`[${activeProfile}].friends`}>{friend =>
             <ListGroup horizontal key={friend} style={{marginTop: `20px`}}>
               <ListGroup.Item horizontal style={{minWidth: '100px', minHeight: '100px'}}>
-                <img src={saw} alt="saw"
-                  width="80"
-                  height="80"
-                  className="d-inline-block align-top"
-                />
+                <img src={`[${friend}].image`} alt="user_image" width="80" height="80"></img>
               </ListGroup.Item>
 
-              <ListGroup.Item style={{minWidth: '300px', minHeight: '100px'}}> 
+              <ListGroup.Item style={{minWidth: '300px', minHeight: '100px'}}>
                 <p align="center"><br></br>
                   <Value src={`[${friend}].name`}>{`${friend}`}</Value>
                 </p>
+                <p hidden>
+                  {friendsOfUser[counter] = `[${friend}].name`}{`${friend}`}
+                </p>
               </ListGroup.Item>
-
+              
               <ListGroup.Item>
                 <Link to="/map">
                   <img src={lupa} alt="lupa"
@@ -98,7 +127,6 @@ const Friends = () => {
               <ListGroup.Item >
                 <Dropdown className='m-4' >
                   <Dropdown.Toggle id="dropdown-basic">
-
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
                     <Dropdown.Item>Delete friend</Dropdown.Item>
@@ -106,8 +134,6 @@ const Friends = () => {
                   </Dropdown.Menu>
                 </Dropdown>
               </ListGroup.Item>
-
-                
             </ListGroup>
           }
           </List>
