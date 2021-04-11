6f8e420da832fac7c1f254c5abcb913885c78dd5
Luis <UO271405@uniovi.es>
2021-04-11 21:38:06 +0200

    Tests of friend list implemented

--- webapp/src/tests/FriendList.test.js
+++ webapp/src/tests/FriendList.test.js
@@ -1,11 +1,9 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import UserList from "../components/FriendList";
+import { render } from "@testing-library/react";
+import FriendList from "../components/FriendList";
 
-test('check that the list of users renders propertly', async () => {
-    //antiguo test de UserList.test.js
-    //const { getByText } = render(<FriendList/>);
-    //const userList = [{name: 'Pablo', email: 'gonzalezgpablo@uniovi.es' }];
-    //const {getByText} = render(<UserList users={userList}/>);
-    //expect(getByText(userList[0].name +' ('+userList[0].email+')')).toBeInTheDocument();
+test("check that we are in the friend list", async () => {
+    const { getByText, getByAltText } = render(<FriendList />);
+    expect(getByAltText("userLogo")).toBeInTheDocument();
+    expect(getByText("Nearby friends")).toBeInTheDocument();
+    expect(getByText("All friends")).toBeInTheDocument();
 });
