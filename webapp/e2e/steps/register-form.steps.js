643d2d91df37d2d0a2ebadf254e0ac5fc75ea6d5
uo2679787 <uo269787@uniovi.es>
2021-04-18 18:48:40 +0200

    Add solid-node-server to e2e

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -2,7 +2,6 @@ const {defineFeature, loadFeature}=require("jest-cucumber");
 const feature = loadFeature("./features/register-form.feature");
 
 defineFeature(feature, test => {
-  
   beforeEach(async () => {
     await global.page.goto("http://localhost:3000", {timeout: 60000});
   });
@@ -24,6 +23,24 @@ defineFeature(feature, test => {
       // blank, app, redirected: get a pod
       await expect(pages.length).toBe(3);
       await expect(await pages[2].url()).toBe("https://solidproject.org/users/get-a-pod");
+    });
+  });
+
+  test("The user wants a local solid pod", ({given, when, then}) => {
+    let username;
+    let password;
+
+    given("A user without a pod", () => {
+      username = "alice";
+      password = "oieyhcdf/&%19823Ayrfcpjh";
+    });
+
+    when("I fill the data in the form and press submit", async () => {
+      await global.page.goto("https://localhost:8443/register", {timeout: 60000});
+    });
+
+    then("I should be redirected to my pod", () => {
+
     });
   });
 });
