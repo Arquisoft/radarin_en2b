cbbe779132395104cab7c624450834fdb9eb4691
uo2679787 <uo269787@uniovi.es>
2021-04-19 00:06:18 +0200

    Fix infinite loop when fetching locations

--- webapp/src/components/MyLocations.js
+++ webapp/src/components/MyLocations.js
@@ -1,18 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSession } from "@inrupt/solid-ui-react";
 import { getLocations, deleteLocation } from "../services/crudPod";
-import Table from "react-bootstrap/Table";
-import { Button } from "@material-ui/core";
+import {Table, Button} from "react-bootstrap";
 
 
 const MyLocations = () => {
     const [locations, setLocations] = useState([]);
     const { session } = useSession();
-    useEffect(() => {
+
+    // https://reactjs.org/docs/hooks-faq.html#is-it-safe-to-omit-functions-from-the-list-of-dependencies
+    const updateTable = useCallback(() => {
         getLocations(session.info.webId).then((list) =>
             setLocations(list)
-        )
-    });
+        );
+    }, [session.info.webId]);
+
+    useEffect(() => {
+        updateTable();
+    }, [updateTable]);
 
 
     var listItems = []
@@ -21,13 +26,14 @@ const MyLocations = () => {
         listItems.push(<tr>
             <td>{splited[0] + ", " +splited[1]}</td>
             <td>{splited[2]}</td>
-            <td><Button variant="contained" data-testid={session.info.webId} onClick={ () => deleteLocation(session.info.webId, location)}>Delete</Button></td>
+            <td><Button variant="contained" data-testid={session.info.webId} onClick={ async () => {await deleteLocation(session.info.webId, location); updateTable();}}>Delete</Button></td>
         </tr>);
     }
     );
 
 
     return <div>
+        <Button block onClick={updateTable}>Refresh</Button>
         <Table striped bordered hover>
             <thead>
                 <tr>
