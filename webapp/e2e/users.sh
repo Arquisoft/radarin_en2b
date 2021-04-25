1ffd51d5aade5f6c92ae18fe44863f336dc46d38
Héctor <60620685+uo269787@users.noreply.github.com>
2021-04-26 00:33:09 +0200

    Remove bashisms in CI user add script

--- webapp/e2e/users.sh
+++ webapp/e2e/users.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
-alice=("alice" "alice" "1234")
-bob=("bob" "bob" "1234")
-charlie=("charlie" "charlie" "1234")
-
-
+#!/bin/sh
 create_user() {
     echo "Creating user <${2}> with username <${1}> and password <${3}>"
     curl --insecure -X POST -d "name=${1}" -d "username=${2}" -d "password=${3}" -d "repeat_password=${3}" https://localhost:8443/api/accounts/new
 }
 
-create_user "${alice[@]}"
-create_user "${bob[@]}"
-create_user "${charlie[@]}"
+create_user "alice" "alice" "1234"
+create_user "bob" "bob" "1234"
+create_user "charlie" "charlie" "1234"
 
 exit 0
