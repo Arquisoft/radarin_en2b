48854bec79e8aac7ee635991cccd9d64494bfd0d
UO270185 <UO270185@uniovi.es>
2021-04-29 23:44:23 +0200

    Added App.test.js

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,13 +1,15 @@
-//import { queryAllByLabelText, render, screen } from "@testing-library/react";
-//import App from "./App";
+import { queryAllByLabelText, render, screen } from "@testing-library/react";
+import App from "./App";
 //import { SessionProvider } from "@inrupt/solid-ui-react";
 
 test("renders login when not logged to a solid pod", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("Loading...")).toBeInTheDocument();
   /*render(
     <SessionProvider sessionId="testing-radarin_en2b">
       <App />
     </SessionProvider>
-  );*/
+  );
   //const { getByText } = render(<App/>);
-  //expect(getByText("Log In")).toBeInTheDocument();
+  //expect(getByText("Log In")).toBeInTheDocument();*/
 });
