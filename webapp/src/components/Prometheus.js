c66292a1f46ae0243df2917ed24101fddea907b6
Andrea García Cernuda <60620700+UO270115@users.noreply.github.com>
2021-05-01 18:55:10 +0200

    Update Prometheus.js

--- webapp/src/components/Prometheus.js
+++ webapp/src/components/Prometheus.js
@@ -1,6 +1,6 @@
 const Prometheus = () => {
     return (
-        <div data-testid="div-embed" className="embed-container" style={{position: "relative", height: "0", overflow: "hidden"}}>
+        <div data-testid="div-embed" className="embed-container">
             <iframe data-testid="iframe-prometheus" className="responsive-iframe" src="https://radarinen2bprometheus.herokuapp.com/" title="prometheus" 
             style={{position: "absolute", top: "0", left: "0", height: "100%", width: "100%"}} frameBorder="0" allowFullScreen/>
         </div>
